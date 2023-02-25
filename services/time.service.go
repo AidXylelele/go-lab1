b4fff191ab28831543dfbca7bfe68953e9f13c1b
Nailgy <ilyha.nali@gmail.com>
2023-02-25 17:57:44 +0200

    feat: added time service

---
+++ services/time.service.go
@@ -0,0 +1,19 @@
+package services
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type JSONTime struct {
+	CURRENT_TIME string `json:"time"`
+}
+
+func GetCurrentTime() string {
+    currentTime := JSONTime{CURRENT_TIME: time.Now().Format(time.RFC3339)}
+    json_data, err := json.Marshal(currentTime)
+    if err != nil {
+        return "Something went wrong"
+    }
+    return string(json_data)
+}
